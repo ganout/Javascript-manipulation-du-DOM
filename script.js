3a5379b8d33562cea06724b6ba30a1fd9886262c
ganout <morgan.bourdillel@gmail.com>
2020-05-21 16:06:38 +0200

    possibility to change paddle background color added

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let paddle = document.querySelector(".paddle");
-document.addEventListener('mousemove', logKey);
+let btnBlack = document.querySelector(".btn-black");
+let btnRed = document.querySelector(".btn-red");
+let btnGreen = document.querySelector(".btn-green");
+let btnOrange = document.querySelector(".btn-orange");
+let btnPurple = document.querySelector(".btn-purple");
+let btnBlue = document.querySelector(".btn-blue");
 
-function logKey(e) {
-    if (e.clientX > 1315) {
-        e.clientX = paddle.style.left = "1265px";
-    } else if (e.clientX < 50) {
-        e.clientX = paddle.style.left = "0";
+document.addEventListener('mousemove', translate);
+
+function translate(position) {
+
+    if (position.clientX > 1315) {
+        position.clientX = paddle.style.marginLeft = "1265px";
+    } else if (position.clientX < 50) {
+        position.clientX = paddle.style.marginLeft = "0";
     } else {
-        paddle.style.left = `${e.clientX - 50}px`;
+        paddle.style.marginLeft = `${position.clientX - 50}px`;
     }
 }
+
+btnBlack.addEventListener('click', function(){paddle.style.backgroundColor = "black"});
+btnRed.addEventListener('click', function(){paddle.style.backgroundColor = "red"});
+btnGreen.addEventListener('click', function(){paddle.style.backgroundColor = "green"});
+btnOrange.addEventListener('click', function(){paddle.style.backgroundColor = "orange"});
+btnPurple.addEventListener('click', function(){paddle.style.backgroundColor = "purple"});
+btnBlue.addEventListener('click', function(){paddle.style.backgroundColor = "blue"});
